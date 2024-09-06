9a03f50f75380df0c93fa3553cbeb1c09e03f5f1
Piyush <piyush.sethi@georgebrown.ca>
2024-09-06 12:53:11 -0400

    created authentication page component and sign in form

--- crwn/src/components/sign-up-form/sign-up-form.component.jsx
+++ crwn/src/components/sign-up-form/sign-up-form.component.jsx
@@ -53,8 +53,8 @@ const SignUpForm = () => {
 
     return(
         <div className='sign-up-container'>
-            <h2>Don't have an account</h2>
-            <h1>Sign up with your email and password</h1>
+            <h2>Don't have an account?</h2>
+            <span>Sign up with your email and password</span>
             <form onSubmit={handleSubmit}>
                 <FormInput label="Display Name" type='text' required onChange={handleChange} name='displayName' value={displayName} />
 
